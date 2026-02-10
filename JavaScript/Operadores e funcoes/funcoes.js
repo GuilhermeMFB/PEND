d441193b03c71d8f5848d4523b3fc74d616d27f8
GuilhermeMFB <guilherme.mfb@gmail.com>
2026-02-10 15:57:58 -0300

    funcoes 10.02.2026

--- JavaScript/Operadores e funcoes/funcoes.js
+++ JavaScript/Operadores e funcoes/funcoes.js
@@ -11,3 +11,117 @@ function somar(a, b) { // Função com parametro e retorno
     return a + b;
 }
 console.log(somar(5, 3));
+
+// atividade
+function imc(peso, altura) {
+    return peso / (altura * altura);
+}
+
+console.log(imc(90, 1.90));
+
+function imparpar(num) {
+    if (num % 2 === 0) {
+        return "Par";
+    } else {
+        return "Ímpar";
+    }
+}
+console.log(imparpar(9));
+
+console.log("******************FUNÇÕES NATIVAS******************");
+
+let agora = new Date();
+console.log(agora);
+
+function mostrarDataHora() {
+  let data = new Date();
+
+  console.log("Dia:", data.getDate());
+  console.log("Mês:", data.getMonth() + 1);
+  console.log("Ano:", data.getFullYear());
+  console.log("Hora:", data.getHours());
+  console.log("Minutos:", data.getMinutes());
+
+  console.log(
+    data.getHours().toString().padStart(2, "0") + ":" +
+    data.getMinutes().toString().padStart(2, "0") + ":" +
+    data.getSeconds().toString().padStart(2, "0") +
+    " - " +
+    data.getDate().toString().padStart(2, "0") + "/" +
+    (data.getMonth() + 1).toString().padStart(2, "0") + "/" +
+    data.getFullYear()
+  );
+}
+
+mostrarDataHora();
+console.log(Math.PI);
+
+function calcularOperacoes(numero) {
+    console.log("Raiz:", Math.sqrt(numero));
+    console.log("Arredondado:", Math.round(numero));
+    console.log("Para cima:", Math.ceil(numero));
+    console.log("Para baixo:", Math.floor(numero));
+    console.log("Quadrado:", Math.pow(numero, 2));
+    console.log("Absoluto:", Math.abs(numero))
+}
+calcularOperacoes(7.8)
+
+console.log("************** Funções string **************");
+function analisartexto(texto) {
+    console.log("Tamanho:", texto.length);
+    console.log("Maíusculo:", texto.toUpperCase());
+    console.log("Minúsculo:", texto.toLowerCase());
+}
+analisartexto("JavaScript");
+
+function oQueFazEssaFuncao(frase) {
+    console.log(frase.includes("JavaScript"));
+}
+oQueFazEssaFuncao("Eu estudo JavaScript");
+
+function eEssaAqui(nome, curso) {
+    return "Aluno: " + nome + " | Curso: " + curso;
+}
+console.log(eEssaAqui("José", "Front-End"));
+
+console.log("*************** Atividades ***************");
+
+function mostrarHoraAtual() {
+  let agora = new Date();
+
+  console.log(
+    "Hora atual:",
+    agora.getHours().toString().padStart(2, "0") + ":" +
+    agora.getMinutes().toString().padStart(2, "0") + ":" +
+    agora.getSeconds().toString().padStart(2, "0")
+  );
+}
+
+function somaEMedia(num1, num2) {
+  let soma = num1 + num2;
+  let media = soma / 2;
+
+  console.log("Soma:", soma);
+  console.log("Média:", media);
+}
+
+function analisarNome(nome) {
+  console.log("Quantidade de letras:", nome.length);
+  console.log("Nome em maiúsculo:", nome.toUpperCase());
+}
+
+function verificarHTML(frase) {
+  if (frase.includes("HTML")) {
+    console.log('A frase contém a palavra "HTML"');
+  } else {
+    console.log('A frase NÃO contém a palavra "HTML"');
+  }
+}
+
+// Chamadas das funções
+mostrarHoraAtual();
+somaEMedia(10, 20);
+analisarNome("Guilherme");
+verificarHTML("Estou estudando HTML e CSS");
+
+
--- JavaScript/Operadores e funcoes/script.js
+++ JavaScript/Operadores e funcoes/script.js
@@ -1,90 +1,84 @@
-console.log("Olá, JavaScript!");
-
+console.log("Operadores e funções em JavaScript");
 let a = 10;
 let b = 3;
 
-console.log("adição: " + (a + b));
-console.log("subtração: " + (a - b));
-console.log("multiplicação: " + (a * b));
-console.log("divisão: " + (a / b));
-console.log("resto da divisão: " + (a % b));
-console.log("potência: " + (a ** b));
+console.log("Adição:", a + b); // Adição
+console.log("Subtração:", a - b); // Subtração 
+console.log("Multiplicação:", a * b); // Multiplicação
+console.log("Divisão:", a / b); // Divisão
+console.log("Resto de divisão:", a % b); // Resto da divisão
+console.log("Potenciação:",a ** b); // Potenciação
 
 let contador = 5;
-contador++;
-console.log (contador);
+contador++; // Incremento
+console.log(contador); // contador = contador + 2
 
-////////////////////////////////////////////////////
-
-let num1 = 7;
-let num2 = 8;
-let num3 = 9;
-let media = (num1 + num2 + num3) / 3;
-console.log ("A média é: " + media);
-
-let dividendo = 29;
-let divisor = 5;
-let resto = dividendo % divisor;
-console.log ("O resto da divisão é: " + resto);
-
-/////////////////////////////////////////////////////
+let v = 15;
+let d = 10;
+let c = 5;
+let media = (v + d + c) / 3;
+console.log("Media:", media);
+let restdiv = v % d;
+console.log("Resto da divisão:", restdiv);
 
 let x = 10;
 let y = "10";
-console.log (x == y);      // true
-console.log (x === y);
-console.log (x != y);      // false
-console.log (x !== y);
+console.log("Igual (valor) ==:", x == y);
+console.log("Igual (valor e tipo) ===:",x === y);
+console.log("Maior ou igual !=:",x != y);
+console.log("Menor ou igual !==:",x !== y);
 
-//////////////////////////////////////////////////////
+let f = 20;
+let g = 10;
+console.log("Maior que >:", f > g);
+console.log("Menor que <:", f < g);
+console.log("Maior ou igual a >=:", f >= g);
+console.log("Menor ou igual a <=:", f <= g);
 
-let idade = 17;
-let maiorDeIdade = idade >= 18;
-if (idade >= 18) {
-    console.log ("É maior de idade.");
+// Exercício
+let idade2 = 17;
+if (idade2 >= 18) {
+    console.log("Você é maior de idade.");
 } else {
-    console.log ("É menor de idade.");
+        console.log("Você é menor de idade.");
+    }
+
+let num1 = 1;
+let num2 = 10;
+if (num1 > num2) {
+    console.log("O primeiro numero é maior");
 }
-
-//////////////////////////////////////////////////////
-
-let numero1 = 15;
-let numero2 = 25;
-let maiorNumero = numero1 > numero2 ? numero1 : numero2;
-console.log("O maior número é: " + maiorNumero);
-
-//////////////////////////////////////////////////////
-
-console.log("op.lógicos:");
-let idade2 = 20;
+    if (num1 == num2) {
+        console.log("Os números são iguais");
+    } 
+    else if (num1 < num2) {
+        console.log("O segundo numero é maior!");
+    }
+// Fim exercicios
+console.log("op.lógicos");
+let idade = 20;
 let temCarteira = true;
-console.log(idade2 >= 18 && temCarteira); // true
+console.log(idade >= 18 && temCarteira);
 
 let chovendo = false;
 let guardaChuva = true;
-console.log(chovendo || guardaChuva); // true
+console.log (chovendo || guardaChuva);
 
 let ligado = false;
-console.log(!ligado); // true
+console.log(!ligado); //true
 
-//////////////////////////////////////////////////////
-
-let nota1 = 8;5;
-let nota2 = 7.5;
-let nota3 = 9.0;
-let mediaFinal = (nota1 + nota2 + nota3) / 3;
-if (mediaFinal >= 7) {
-    console.log("Aprovado com média: " + mediaFinal);
+let nota = 7;
+let freq = 80;
+if (nota >= 7 && freq >= 75) {
+    console.log("Você esta aprovado!");
 } else {
-    console.log("Reprovado com média: " + mediaFinal);
+    console.log("Você esta reprovado!")
 }
 
-//////////////////////////////////////////////////////
-
-let login = "true"
-let token = false
-if (login === true || token == true) {
-    console.log("Acesso permitido");
+let login = false;
+let token = true  ;
+if (login || token) {
+    console.log("Você pode acessar!!")
 } else {
-    console.log("Acesso negado");
+    console.log("Você não pode acessar!!")
 }
